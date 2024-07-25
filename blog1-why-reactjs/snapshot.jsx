d805de27577a10c21f029f2903a68b2db558f6dd
Agboola Vincent <agboolavin65@gmail.com>
2024-07-25 12:59:11 +0100

    First commit

---
+++ blog1-why-reactjs/snapshot.jsx
@@ -0,0 +1,6 @@
+// This is our basic component 'Welcome'
+const Welcome = (props) => {
+  return <h1>Hello, {props.name}!</h1>;
+};
+
+export default Welcome;
---
+++ blog1-why-reactjs/snapshot2.jsx
@@ -0,0 +1,11 @@
+/* Using the Welcome component */
+const Homepage = () => {
+  return (
+    <div>
+      <Welcome name="Alice" />
+      <Welcome name="Bob" />
+    </div>
+  );
+};
+
+export default Homepage;
